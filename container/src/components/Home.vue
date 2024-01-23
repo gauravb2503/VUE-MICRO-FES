eee8433b7ef1b67e47e32dfa38ec4874abbfb262
gaurav2503b <gaurav.b@simformsolutions.com>
2024-01-23 13:00:01 +0530

    added support for running the build and dev server from the root application

--- container/src/components/Home.vue
+++ container/src/components/Home.vue
@@ -1,6 +1,19 @@
 <script setup>
 import { ref } from 'vue'
+import {defineAsyncComponent} from "vue";
+const Cart = defineAsyncComponent(() => import("cart/Cart"));
+const Products = defineAsyncComponent(() => import("products/Products"));
 
 </script>
 
-<template>This is Home page</template>
+<template>
+    <div><h1>Shopping App</h1></div>
+    <hr>
+    <div><h3>Product Module</h3></div>
+    <Products />
+    <hr>
+    <div>
+        <h3>Cart Module</h3>
+    </div>
+    <Cart />
+</template>
