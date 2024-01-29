39f670aa2e1295c378d22dc659144d80f4238bb0
gauravb2503 <gaurav.b@simformsolutions.com>
2024-01-29 11:03:18 +0530

    fixed the routing issue

--- container/src/router/router.js
+++ container/src/router/router.js
@@ -1,13 +1,9 @@
 // router.js
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home.vue';
-import Products from '../components/Products.vue';
-import Cart from '../components/Cart.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/products', component: Products },
-  { path: '/cart', component: Cart },
 ];
 
 const router = createRouter({
